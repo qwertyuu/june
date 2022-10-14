3dcf2c2c31acbcc7f1e5b9366619931e09dafbc6
LilyCodez <ordinallisting@gmail.com>
2022-10-14 00:34:18 -0400

    Tests directory cleanup

--- tests/Tests.cpp
+++ tests/Tests.cpp
@@ -88,10 +88,10 @@ int main() {
 
 	//RunStdLibTest(LIB_SRC("addtwo"));
 	
-	RunTest(SRC("main1"), 0);
-	RunTest(SRC("main2"), 55);
-	RunTest(SRC("exprs1"), 214 + 41 / 2 - 663 * 3);
-	RunTest(SRC("exprs2"), []() {
+	RunTest(SRC("main/Main1.june"), 0);
+	RunTest(SRC("main/Main2.june"), 55);
+	RunTest(SRC("exprs/Exprs1.june"), 214 + 41 / 2 - 663 * 3);
+	RunTest(SRC("exprs/Exprs2.june"), []() {
 		s32 b = 22;
 		s32 sum = 44 * 3 + 55 - 421 * b;
 		s32 g = 4;
@@ -100,13 +100,13 @@ int main() {
 		sum = sum * sum;
 		return sum;
 		}());
-	RunTest(SRC("exprs3"), []() {
+	RunTest(SRC("exprs/Exprs3.june"), []() {
 		s32 a = 22;
 		s32 b = a << 6;
 		s32 c = b % 20;
 		return c + (312312 >> 3) ^ 7;
 		}());
-	RunTest(SRC("exprs4"), []() {
+	RunTest(SRC("exprs/Exprs4.june"), []() {
 		s32 g = 0;
 		++g;
 		--g;
@@ -117,13 +117,13 @@ int main() {
 		g = g++;
 		return g;
 		}());
-	RunTest(SRC("functioncall1"), 523);
-	RunTest(SRC("functioncall2"), 425 + 22 + 41 + 53);
-	RunTest(SRC("loops1"), 10);
-	RunTest(SRC("loops2"), 10);
-	RunTest(SRC("loops3"), 10);
-	RunTest(SRC("loops4"), 15);
-	RunTest(SRC("loops5"), []() {
+	RunTest(SRC("functioncall/FunctionCall1.june"), 523);
+	RunTest(SRC("functioncall/FunctionCall2.june"), 425 + 22 + 41 + 53);
+	RunTest(SRC("loops/Loops1.june"), 10);
+	RunTest(SRC("loops/Loops2.june"), 10);
+	RunTest(SRC("loops/Loops3.june"), 10);
+	RunTest(SRC("loops/Loops4.june"), 15);
+	RunTest(SRC("loops/Loops5.june"), []() {
 		s32 sum = 0;
 		for (s32 i = 0; ; i++) {
 			if (i > 50) {
@@ -136,36 +136,36 @@ int main() {
 		}
 		return sum;
 		}());
-	RunTest(SRC("loops6"), 124 + 346 + 22 + 15 + 77);
-	RunTest(SRC("loops7"), 214 + 22 + 55 + 21 + 553 + 2 + 52 + 14);
-	RunTest(SRC("fixedarrays1"), 21 + 55 + 11 + 56 + 3);
-	RunTest(SRC("fixedarrays2"), 412 + 21 + 5 + 6 + 4);
-	RunTest(SRC("fixedarrays3"), 41 + 36 + 412 + 121 + 45 + 56);
-	RunTest(SRC("fixedarrays4"), 214 + 452 + 12 + 32 + 11);
-	RunTest(SRC("fixedarrays5"), 253 + 23 + 13);
-	RunTest(SRC("fixedarrays6"), 5 * 8);
-	RunTest(SRC("fixedarrays7"), 1 + 2 + 3 + 4 + 5);
-	RunTest(SRC("fixedarrays8"), 12 + 465 + 5437 + 12 + 61);
+	RunTest(SRC("loops/Loops6.june"), 124 + 346 + 22 + 15 + 77);
+	RunTest(SRC("loops/Loops7.june"), 214 + 22 + 55 + 21 + 553 + 2 + 52 + 14);
+	RunTest(SRC("fixedarrays/FixedArrays1.june"), 21 + 55 + 11 + 56 + 3);
+	RunTest(SRC("fixedarrays/FixedArrays2.june"), 412 + 21 + 5 + 6 + 4);
+	RunTest(SRC("fixedarrays/FixedArrays3.june"), 41 + 36 + 412 + 121 + 45 + 56);
+	RunTest(SRC("fixedarrays/FixedArrays4.june"), 214 + 452 + 12 + 32 + 11);
+	RunTest(SRC("fixedarrays/FixedArrays5.june"), 253 + 23 + 13);
+	RunTest(SRC("fixedarrays/FixedArrays6.june"), 5 * 8);
+	RunTest(SRC("fixedarrays/FixedArrays7.june"), 1 + 2 + 3 + 4 + 5);
+	RunTest(SRC("fixedarrays/FixedArrays8.june"), 12 + 465 + 5437 + 12 + 61);
 	RunTest(SRC("multifiles"), 65 + 4);
-	RunTest(SRC("records1"), 14 + 15);
-	RunTest(SRC("records2"), 1415 + 156 + 76534);
-	RunTest(SRC("records3"), 124 + 66 + 25);
-	RunTest(SRC("records4"), 56 + 14);
-	RunTest(SRC("records5"), 325 + 15 + 325 + 15);
-	RunTest(SRC("records6"), 5 * 5 * (25 + 55));
-	RunTest(SRC("records7"), 55);
-	RunTest(SRC("records8"), 138);
-	RunTest(SRC("sizeof"), 4 + 1 + 4 + 4 + 8 + 8);
-	RunTest(SRC("this"), 252);
-	RunTest(SRC("inferedtypes"), 11 + 11 + 365 + 214 + 14 + 65 + 11);
-	RunTest(SRC("globals1"), 44);
-	RunTest(SRC("globals2"), 32 + 154 + 32);
-	RunTest(SRC("globals3"), 22 + 1111 + 34 + 66);
-	RunTest(SRC("globals4"), 154 + 154 + 11 + 11 + 11);
-	RunTest(SRC("varfunccall1"), 11);
-	RunTest(SRC("varfunccall2"), 15323);
-	RunTest(SRC("varfunccall3"), 6578 + 12);
-	RunTest(SRC("varfunccall4"), 1241 + 778);
+	RunTest(SRC("records/Records1.june"), 14 + 15);
+	RunTest(SRC("records/Records2.june"), 1415 + 156 + 76534);
+	RunTest(SRC("records/Records3.june"), 124 + 66 + 25);
+	RunTest(SRC("records/records4"), 56 + 14);
+	RunTest(SRC("records/Records5.june"), 325 + 15 + 325 + 15);
+	RunTest(SRC("records/Records6.june"), 5 * 5 * (25 + 55));
+	RunTest(SRC("records/Records7.june"), 55);
+	RunTest(SRC("records/Records8.june"), 138);
+	RunTest(SRC("othertests/Sizeof.june"), 4 + 1 + 4 + 4 + 8 + 8);
+	RunTest(SRC("othertests/This.june"), 252);
+	RunTest(SRC("othertests/InferedTypes.june"), 11 + 11 + 365 + 214 + 14 + 65 + 11);
+	RunTest(SRC("globals/Globals1.june"), 44);
+	RunTest(SRC("globals/Globals2.june"), 32 + 154 + 32);
+	RunTest(SRC("globals/Globals3.june"), 22 + 1111 + 34 + 66);
+	RunTest(SRC("globals/Globals4.june"), 154 + 154 + 11 + 11 + 11);
+	RunTest(SRC("varfunccall/VarFuncCall1.june"), 11);
+	RunTest(SRC("varfunccall/VarFuncCall2.june"), 15323);
+	RunTest(SRC("varfunccall/VarFuncCall3.june"), 6578 + 12);
+	RunTest(SRC("varfunccall/VarFuncCall4.june"), 1241 + 778);
 	//RunTest(SRC("playground"), 0);
 
 	if (Succeeded + Failed > 0) {
